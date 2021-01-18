59d40856d4d83d4b3799e1fca5af9e880fd836f1
Gursharan Kaur <45887638+GursharanKaur08@users.noreply.github.com>
2021-01-18 22:16:34 +0530

    Add files via upload

---
+++ Shutdown Restart Your PC/shutdown_restart.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+class System:
+
+    def __init__(self,input_char):
+        self.input_char=input_char
+
+    def restart(self):
+        os.system('shutdown /r')
+
+    def shutdown(self):
+        os.system('shutdown /s')
+
+    def exit_prg(self):
+        sys.exit()
+
+print('Enter \'r\' for restart')
+print('Enter \'s\' for shutdown')
+print('Enter any char to exit from the program.')
+input_char = input('Enter r|s: ')
+
+system = System(input_char)
+if input_char=='r':
+    system.restart()
+elif input_char=='s':
+    system.shutdown()
+else:
+    system.exit_prg()
